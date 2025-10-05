e3d9c725bdda5e9afcfae67e57248784420e120c
Tatjana Tomek <tatjana.tomek@embl.de>
2025-10-05 02:09:56 +0200

    replace read_csv() by read.table()

--- visium/01_CNV_calling/small_random_scripts/merge_csv_files.R
+++ visium/01_CNV_calling/small_random_scripts/merge_csv_files.R
@@ -3,7 +3,9 @@ library(dplyr)
 library(readr)
 
 # dir, patterns of csv files to merge
-d = "/g/saka/Tatjana/data/01_CNV_analysis/01_infercnv_output/spotbased/all_pat_analyzed_spotbased_BMPN_0.3_NEW_prelim_growth2/20251004/01_CNVs_GROUPED_PER_PATIENT/"
+d = "/g/saka/Tatjana/data/01_CNV_analysis/01_infercnv_output/spotbased/all_pat_analyzed_spotbased_BMPN_0.3_NEW_prelim_growth2/20251004/"
+i = paste0(d, "01_UNMERGED")
+o = paste0(d, "02_MERGED")
 patterns = c("LN0025",
              "LN0027",
              "LN0438",
@@ -15,15 +17,17 @@ patterns = c("LN0025",
 # define funtion
 merge_csv = function(dir, pattern, output) {
   files = list.files(dir, pattern, full.names = TRUE)
-  dfs = lapply(files, read_csv)
+  dfs = lapply(files, function(f) {
+    read.table(f, header = TRUE, sep = ",")
+  })
   merged = bind_rows(dfs)
   write_csv(merged, output)
 }
 
 # iter over all patient ids
 lapply(patterns, function(pat) {
-  out = paste0(d, "/", pat, "_merged.csv")
-  merge_csv(d, pat, out)
+  out = paste0(o, "/", pat, "_merged.csv")
+  merge_csv(i, pat, out)
 })
 
 
